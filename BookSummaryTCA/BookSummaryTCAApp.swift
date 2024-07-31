cf0a016388eab95b7d2feed0d85c44d4dca83a78
Taras Tarasenko <thetarasenko.t@gmail.com>
2024-07-31 15:37:40 +0300

    Initial Commit

---
+++ BookSummaryTCA/BookSummaryTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookSummaryTCAApp.swift
+//  BookSummaryTCA
+//
+//  Created by Taras Tarasenko on 31.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct BookSummaryTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
